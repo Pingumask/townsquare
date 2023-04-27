de065e4a9fb831ffc0d1920d930cd0a58c7e5f41
MRegnard <62838312+MRegnard@users.noreply.github.com>
2023-04-27 19:42:46 +0200

    Update Vote.vue

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -10,7 +10,7 @@
       <em>{{ nominee.name }}</em
       >!
       <br />
-      <em class="blue" v-if="!grimoire.isOrganVoteMode || nominee.role.team == 'traveler' || !session.isSpectator">
+      <em class="blue" v-if="!grimoire.isOrganVoteMode || nominee.role.team == 'traveler' || session.isToryteller">
         {{ voters.length }} {{ locale.vote.votes }}
       </em>
       <em class="blue" v-else>
