a2e9d3b1e71fa3eace46643a2abbaf235bd0728b
MRegnard <62838312+MRegnard@users.noreply.github.com>
2024-04-26 17:43:12 +0200

    Changing some typos with the { }

--- src/components/TownSquare.vue
+++ src/components/TownSquare.vue
@@ -191,11 +191,11 @@ export default {
     ...mapState(["grimoire", "roles", "session", "locale"]),
     ...mapState("players", ["players", "bluffs", "fabled"]),	
     firstMessage() {
-	  return JSON.stringify(this.locale.modal.nightOrder.firstNight) ;
-	},	
+      return JSON.stringify(this.locale.modal.nightOrder.firstNight) ;
+    },	
     otherMessage() {
-	  return JSON.stringify(this.locale.modal.nightOrder.otherNights) ;
-	}
+      return JSON.stringify(this.locale.modal.nightOrder.otherNights) ;
+    }
   },
   data() {
     return {
