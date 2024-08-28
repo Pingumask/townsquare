d014370b56f77c75a39b7ebd860c8eadd8c982c0
MRegnard <62838312+MRegnard@users.noreply.github.com>
2024-08-28 10:44:49 +0200

    Correcting some spaces typos

--- src/components/modals/EditionModal.vue
+++ src/components/modals/EditionModal.vue
@@ -194,45 +194,45 @@ export default {
 	  
       // set fabled
       const fabled = [];
-	  var djinnAdded = false ;
-	  var djinnNeeded = false ;
-	  var bootleggerAdded = false ;
-	  var bootleggerNedded = false ;
+      var djinnAdded = false ;
+      var djinnNeeded = false ;
+      var bootleggerAdded = false ;
+      var bootleggerNedded = false ;
 	  
       roles.forEach((role) => {
         if (this.$store.state.fabled.has(role.id || role)) {
           fabled.push(this.$store.state.fabled.get(role.id || role));
-		  if((role.id || role) == "djinn") {
-		    djinnAdded = true ;
-		  }
-		  else if((role.id || role) == "bootlegger") {
-		    bootleggerAdded = true ;
-		  }
+          if((role.id || role) == "djinn") {
+            djinnAdded = true ;
+          }
+          else if((role.id || role) == "bootlegger") {
+            bootleggerAdded = true ;
+          }
         }
-		/* If the role isn't fabled, but detected as custom, we will need a Bootlegger
-		 * NB: The actual version isn't perfect, since they only detect roles with an image.
-		 * The code will could be changed later, when all non-custom roles will have an attribute "edition"
-		 */
+        /* If the role isn't fabled, but detected as custom, we will need a Bootlegger
+         * NB: The actual version isn't perfect, since they only detect custom roles with an image or with the argument "edition":"custom".
+         * The code will could be changed later, when all non-custom roles will have an attribute "edition"
+         */
         else if (role.edition == "custom"  ||  role.image) {
           bootleggerNedded = true ;
         }
-		
-		// If the role isn't fabled, neither custom, and if we neither added a Djinn neither planned to add a Djinn, we look if this role is jinxed
-		else if(!djinnAdded && !djinnNeeded && (this.jinxes.get(role.id))) {
-		  this.jinxes.get(role.id).forEach((reason, second) => {
+          
+        // If the role isn't fabled, neither custom, and if we neither added a Djinn neither planned to add a Djinn, we look if this role is jinxed
+        else if(!djinnAdded && !djinnNeeded && (this.jinxes.get(role.id))) {
+          this.jinxes.get(role.id).forEach((reason, second) => {
             if (this.roles.get(second)) {
               djinnNeeded = true ;
             }
           });
-		}
+        }
       });
-	  
-	  if (djinnNeeded && !djinnAdded) {
-	    fabled.push(this.$store.state.fabled.get("djinn"));
-	  }
-	  if (bootleggerNedded && !bootleggerAdded) {
-	    fabled.push(this.$store.state.fabled.get("bootlegger"));
-	  }
+      
+      if (djinnNeeded && !djinnAdded) {
+        fabled.push(this.$store.state.fabled.get("djinn"));
+      }
+      if (bootleggerNedded && !bootleggerAdded) {
+        fabled.push(this.$store.state.fabled.get("bootlegger"));
+      }
 	  
       this.$store.commit("players/setFabled", { fabled });
     },
