d058cffe6a0de6c4b1fafaeeec0ec09867ec0af7
Pingumask <pingumask@elanta.fr>
2025-10-11 04:46:53 +0200

    MAINT > Organized types

---
+++ src/types/players.d.ts
@@ -0,0 +1,30 @@
+import type { Role } from './roles';
+
+// Player reminder interface
+export interface Reminder {
+  id: string;
+  role: string;
+  name: string;
+  image?: string;
+  imageAlt?: string;
+}
+
+// Player interface
+export interface Player {
+  name: string;
+  id: string;
+  role: Role;
+  alignment?: "good" | "evil" | null;
+  reminders: Reminder[];
+  voteToken: boolean;
+  isDead: boolean;
+  pronouns: string;
+  isMarked?: boolean;
+}
+
+// Players state
+export interface PlayersState {
+  players: Player[];
+  fabled: Role[];
+  bluffs: Role[];
+}
