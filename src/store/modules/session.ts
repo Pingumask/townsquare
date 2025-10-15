5ef98e100de13e731b9945b37da4636cf08ed1ed
MRegnard <62838312+MRegnard@users.noreply.github.com>
2025-10-15 14:03:30 +0200

    Making vote history unaccessible for player by default
    + Makes that the player still save the votes, in case the history becomes accessible later.

--- src/store/modules/session.ts
+++ src/store/modules/session.ts
@@ -40,7 +40,7 @@ const state = (): SessionState => ({
   voteHistory: [],
   markedPlayer: -1,
   playerForSpecialVote: -1,
-  isVoteHistoryAllowed: true,
+  isVoteHistoryAllowed: false,
   isRolesDistributed: false,
   isSecretVote: false,
   gamePhase: "pregame",
@@ -248,7 +248,6 @@ const mutations = {
   ) {
     const { players, isOrganVoteMode = false, localeTexts } = payload;
 
-    if (!state.isVoteHistoryAllowed && state.isSpectator) return;
     if (
       !isActiveNomination(state.nomination) ||
       state.lockedVote <= players.length
