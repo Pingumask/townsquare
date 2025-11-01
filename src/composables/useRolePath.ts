b4300be72f67fae79af67173ef5603ad5fb786c7
MRegnard <62838312+MRegnard@users.noreply.github.com>
2025-11-01 16:59:29 +0100

    Allowing marking an alignment change on custom roles

--- src/composables/useRolePath.ts
+++ src/composables/useRolePath.ts
@@ -1,12 +1,33 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
-import { Role } from "../types";
+import { Role, Player } from "../types";
+
+
+function default_alignment(team, alignment) {
+  if (alignment == "good") {
+    return (team == "townsfolk" || team == "outsider");
+  }
+  else if (alignment == "evil") {
+    return (team == "minion" || team == "demon");
+  }
+  return true;
+}
+
+
+function teamIcon(team, alignment) {  
+  
+  if (!default_alignment(team, alignment)) {
+    return new URL(`../assets/icons/${team}_${alignment}.png`, import.meta.url).href;
+  }
+  
+  return new URL(`../assets/icons/${team}.png`, import.meta.url).href;
+}
 
 export function useRolePath() {
   const store = useStore();
   const grimoire = computed(() => store.state.grimoire);
 
-  const rolePath = (role: Role) => {
+  const rolePath = (role: Role, alignment=null) => {
     if (
       [
         "dusk",
@@ -21,10 +42,41 @@ export function useRolePath() {
       return new URL(`../assets/icons/${role.id}.png`, import.meta.url).href;
     }
     if (role.image && grimoire.value.isImageOptIn) {
-      return role.image;
+      if (Array.isArray(role.image)) {
+        switch(role.image.length) {
+          case 0 :
+            return teamIcon(role.team, alignment);
+            break;
+          case 1 :
+            return role.image[0];
+            break;
+          case 2 :
+            if (default_alignment(role.team, alignment)) {
+              return role.image[0];
+            }
+            else {
+              return role.image[1];
+            }
+            break;
+          default :
+            if (alignment == "good") {
+              return role.image[1];
+            }
+            else if(alignment == "evil") {
+              return role.image[2];
+            }
+            else {
+              return role.image[0];
+            }
+            break;
+        }
+      } 
+      else {
+        return role.image; 
+      }
     }
     if (role.image && !grimoire.value.isImageOptIn) {
-      return new URL(`../assets/icons/${role.team}.png`, import.meta.url).href;
+      return teamIcon(role.team, alignment);
     }
     return new URL(`../assets/icons/${role.id}.svg`, import.meta.url).href;
   };
