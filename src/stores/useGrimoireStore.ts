054e0c1c534c5bd4c8b6380cd2c91ece701b6e23
MRegnard <adresse0poubelle@hotmail.com>
2026-01-24 12:56:10 +0100

    Correcting the check

--- src/stores/useGrimoireStore.ts
+++ src/stores/useGrimoireStore.ts
@@ -265,7 +265,7 @@ export const useGrimoireStore = defineStore("grimoire", {
           role.id = role.id.replace(/[^a-z]/gi, '').toLowerCase();
 
           // Correcting the Traveller type if needed
-          if(role.team == "traveller") {
+          if(role.team === "traveller") {
             role.team = "traveler";
           }
 
